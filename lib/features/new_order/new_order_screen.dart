579d1b08d88891c15a44ee7ba21de8cda8d360e9
artem.soglaev <artem.soglaev@mail.ru>
2024-04-28 00:05:04 +0300

    add loading

--- lib/features/new_order/new_order_screen.dart
+++ lib/features/new_order/new_order_screen.dart
@@ -17,12 +17,18 @@ class _NewOrderScreenState extends State<NewOrderScreen> {
 
   String? errorText;
 
+  bool isLoading = false;
+
   OrderLocalModel? orderModel;
 
   TextEditingController controller = TextEditingController();
 
   Future<void> getStatus() async {
     try {
+      setState(() {
+        isLoading = true;
+      });
+
       final res = await getIt<OrderRepository>().getOrderStatusById(controller.text);
 
       setState(() {
@@ -33,52 +39,61 @@ class _NewOrderScreenState extends State<NewOrderScreen> {
     } on Exception catch (e) {
       errorText = (e as FormatException).message;
     }
+    setState(() {
+      isLoading = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15),
-          child: Column(
-            children: [
-              if (errorText != null) ...[
-                Text(
-                  errorText!,
-                  style: const TextStyle(color: Colors.red),
+        appBar: AppBar(),
+        body: Stack(
+          children: [
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Column(
+                  children: [
+                    if (errorText != null) ...[
+                      Text(
+                        errorText!,
+                        style: const TextStyle(color: Colors.red),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                    ],
+                    const Text(
+                      'Поиск заказа',
+                      style: TextStyle(fontWeight: FontWeight.w700, fontSize: 32),
+                    ),
+                    TextFormField(
+                      controller: controller,
+                      decoration:
+                          const InputDecoration(prefixIcon: Icon(Icons.search), hintText: 'Введите номер заказа'),
+                    ),
+                    DefaultAppButton(
+                      onTap: getStatus,
+                      content: const Text('Получить информацию о заказе'),
+                    ),
+                    const SizedBox(height: 20),
+                    if (orderModel != null)
+                      Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: OrderWidget(
+                          order: orderModel!,
+                        ),
+                      )
+                  ],
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
-              ],
-              const Text(
-                'Поиск заказа',
-                style: TextStyle(fontWeight: FontWeight.w700, fontSize: 32),
               ),
-              TextFormField(
-                controller: controller,
-                decoration: const InputDecoration(prefixIcon: Icon(Icons.search), hintText: 'Введите номер заказа'),
-              ),
-              DefaultAppButton(
-                onTap: () {
-                  getStatus();
-                },
-                content: const Text('Получить информацию о заказе'),
-              ),
-              const SizedBox(height: 20),
-              if (orderModel != null)
-                Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: OrderWidget(
-                    order: orderModel!,
-                  ),
-                )
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+            if (isLoading)
+              const Center(
+                child: CircularProgressIndicator(),
+              )
+          ],
+        ));
   }
 }
